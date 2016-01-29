733db6013ef703d8962510b2f514619c097e29c3
Fellipe Sales <fellipesales@gmail.com>
2016-01-29 01:09:39 -0200

    estrutura com bean validation cdi e jsf

--- src/main/java/br/com/programafidelidade/model/Empresa.java
+++ src/main/java/br/com/programafidelidade/model/Empresa.java
@@ -2,69 +2,94 @@ package br.com.programafidelidade.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Empresa {
-	
+
 	private int id;
 	private String CNPJ;
 	private String nome;
 	private String contato;
 	private String login;
 	private String senha;
-	
 
 	private List<Cartao> listaCartao = new ArrayList<Cartao>();
 	private List<Cliente> listaCliente = new ArrayList<Cliente>();
-	
-	
+
+	@Id
+	@GeneratedValue
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	@Column(nullable = false)
 	public String getCNPJ() {
 		return CNPJ;
 	}
+
 	public void setCNPJ(String cNPJ) {
 		CNPJ = cNPJ;
 	}
+
+	@Column(nullable = false, length = 80)
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Column(nullable = false, length = 20)
 	public String getContato() {
 		return contato;
 	}
+
 	public void setContato(String contato) {
 		this.contato = contato;
 	}
+
 	public List<Cartao> getListaCartao() {
 		return listaCartao;
 	}
+
 	public void setListaCartao(List<Cartao> listaCartao) {
 		this.listaCartao = listaCartao;
 	}
+
 	public List<Cliente> getListaCliente() {
 		return listaCliente;
 	}
+
 	public void setListaCliente(List<Cliente> listaCliente) {
 		this.listaCliente = listaCliente;
 	}
+
+	@Column( nullable = false, length = 32)
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	@Column(nullable = false, length = 40)
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
+
 		this.login = login;
 	}
 
-	
 }
